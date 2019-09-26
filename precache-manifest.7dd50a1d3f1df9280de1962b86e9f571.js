957f92f7c4aadbd3e9f360f44e0c0deaa7c48bf3
cucko <cucko@webthink.com>
2019-09-26 12:18:02 +0200

    Updates

---
+++ precache-manifest.7dd50a1d3f1df9280de1962b86e9f571.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db131b162426e8cdd5c4fe3c5aca829e",
+    "url": "/MauMau/index.html"
+  },
+  {
+    "revision": "b5713d5585f42e2bd5fe",
+    "url": "/MauMau/static/css/main.81adfb9e.chunk.css"
+  },
+  {
+    "revision": "24ea2645f7536642847f",
+    "url": "/MauMau/static/js/2.2d2acc9c.chunk.js"
+  },
+  {
+    "revision": "b5713d5585f42e2bd5fe",
+    "url": "/MauMau/static/js/main.79262399.chunk.js"
+  },
+  {
+    "revision": "b7da44abd8b51ed1e072",
+    "url": "/MauMau/static/js/runtime-main.69e15742.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e10feaf335c105668343301f18fab8a.js"
+  "/MauMau/precache-manifest.7dd50a1d3f1df9280de1962b86e9f571.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MauMau/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
