cc0d14a31263fdf980deea3755894d36d6243565
cucko <cucko@webthink.com>
2019-09-26 09:10:32 +0200

    Updates

--- precache-manifest.f992ff555dd3e5861eff9bb5bde2ba8c.js
+++ precache-manifest.f992ff555dd3e5861eff9bb5bde2ba8c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f7770c7d5f0a072a023c3bbc4d22750",
+    "revision": "3531a2d5f69c23ff082fe3d88de8148b",
     "url": "/MauMau/index.html"
   },
   {
-    "revision": "6e7fc6436c0ffd25f692",
+    "revision": "76618334e5198e81f461",
     "url": "/MauMau/static/css/main.7bdc299b.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MauMau/static/js/2.965ac617.chunk.js"
   },
   {
-    "revision": "6e7fc6436c0ffd25f692",
-    "url": "/MauMau/static/js/main.509c0b51.chunk.js"
+    "revision": "76618334e5198e81f461",
+    "url": "/MauMau/static/js/main.3bb0cf32.chunk.js"
   },
   {
     "revision": "b7da44abd8b51ed1e072",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MauMau/precache-manifest.ad1e8793ac570df6c8a3e3bab1fc2a38.js"
+  "/MauMau/precache-manifest.f992ff555dd3e5861eff9bb5bde2ba8c.js"
 );
 
 self.addEventListener('message', (event) => {
