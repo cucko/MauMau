2498bfcb3c3ab77cfe0efed60cfdb5dbf0d4b32a
cucko <cucko@webthink.com>
2019-09-30 08:54:36 +0200

    Updates

--- precache-manifest.e67aafa9155a8b4f52b5ee01620765ce.js
+++ precache-manifest.e67aafa9155a8b4f52b5ee01620765ce.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d5f897deaabeb4a9ad33abd2c940908c",
+    "revision": "ff6933f6b0cd2290d766763ceff4a4ad",
     "url": "/MauMau/index.html"
   },
   {
-    "revision": "85f1727a64f7dc7492e8",
-    "url": "/MauMau/static/css/main.81adfb9e.chunk.css"
+    "revision": "14abe51ff398f1ffea10",
+    "url": "/MauMau/static/css/main.c6c5226c.chunk.css"
   },
   {
     "revision": "38a01f3c36155d25bade",
     "url": "/MauMau/static/js/2.39de781c.chunk.js"
   },
   {
-    "revision": "85f1727a64f7dc7492e8",
-    "url": "/MauMau/static/js/main.e1cfa363.chunk.js"
+    "revision": "14abe51ff398f1ffea10",
+    "url": "/MauMau/static/js/main.e511ffd9.chunk.js"
   },
   {
     "revision": "b7da44abd8b51ed1e072",
